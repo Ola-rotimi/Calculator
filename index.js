fa2192ad8fa5980753861d8b7191549eca5fbc45
Olarotimi Adamson <olarotimiadamson@yahoo.com>
2022-08-11 12:03:54 +0100

    Calculator Still  in Progress

---
+++ index.js
@@ -0,0 +1,13 @@
+let displays = document.querySelector('#display')
+
+let buttons = document.getElementsByTagName('button')
+
+// button => {
+//     button.addEventListener('click' (calculate => {
+//         if (button === 'C')
+//         displays.innerHTML = ''
+//     }
+//     ))
+// }
+
+console.log(buttons)
